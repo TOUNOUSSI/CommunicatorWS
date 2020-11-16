89bf1e5c3db9de49fe36c1c7612263be0438297b
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2020-11-16 23:13:00 +0100

    update POM & properties file

---
+++ src/main/java/com/gmart/api/CommunicatorApplication.java
@@ -0,0 +1,13 @@
+package com.gmart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicatorApplication.class, args);
+	}
+
+}
